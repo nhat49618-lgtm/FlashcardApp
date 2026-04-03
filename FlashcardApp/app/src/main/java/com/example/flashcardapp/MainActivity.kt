ce6547984c202e9cc95a1d43aa2e44df5fe2bf7f
nhat49618-lgtm <nhat49618@gmail.com>
2026-04-03 16:08:06 +0700

    git add . git commit -m "feat: add test change for PR"

--- FlashcardApp/app/src/main/java/com/example/flashcardapp/MainActivity.kt
+++ FlashcardApp/app/src/main/java/com/example/flashcardapp/MainActivity.kt
@@ -36,6 +36,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name!",
         modifier = modifier
     )
+    println("Hello Flashcard")
 }
 
 @Preview(showBackground = true)
